a6ba30cc6a00d9692c0559935daa8b4eb0ff00de
Joseph Elsherbini <joseph.elsherbini@gmail.com>
2016-01-26 19:44:03 -0500

    use libnotify for ubuntu

--- lib/terminal_notifier_logger.rb
+++ lib/terminal_notifier_logger.rb
@@ -1,9 +1,11 @@
 class TerminalNotifierLogger
   def notify_start task
-    TerminalNotifier.notify('Pomodoro started', title: 'Pomotxt', sound: 'Glass')
+    Libnotify.show:summary =>
+        "Pomodoro started"
   end
 
   def notify_completed task
-    TerminalNotifier.notify('Pomodoro completed!', title: 'Pomotxt', sound: 'Glass')
+        Libnotify.show:summary =>
+        "Pomodoro comlpeted"
   end
 end
